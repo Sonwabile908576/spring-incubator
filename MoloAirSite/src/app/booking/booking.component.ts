52f41c30af4d2c6e1d762692b7bea04883d4e2f6
Sonwabile Balite <sonwabile.balite@entelect.co.za>
2023-03-16 19:36:48 +0200

    started with booking

--- MoloAirSite/src/app/booking/booking.component.ts
+++ MoloAirSite/src/app/booking/booking.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css']
 })
-export class BookingComponent {
+export class BookingComponent implements OnInit {
 
+  constructor() { }
+  //get flightService
+  //get customerService
+  //get bookingService
+
+  //show flight details
+  //show customer details
+  // when user clicks on book button
+  // call bookingService.bookFlight(flightId, customerId)
+  // if booking is successful, show success message
+  // if booking is not successful, show error message
+
+  /*
+  - after all these stuff, make customer profile page
+  - show customer bookings
+  - fix flight search feature
+  - sleep
+
+  
+  */
+
+  ngOnInit(): void {
+  }
 }
